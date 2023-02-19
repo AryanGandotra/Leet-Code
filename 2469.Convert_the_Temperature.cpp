d131e0bb428f28bfac7cb66c0a3d9b8835e301cb
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2023-02-19 15:37:35 +0530

    Create 2469.Convert_the_Temperature.cpp

---
+++ 2469.Convert_the_Temperature.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+public:
+    vector<double> convertTemperature(double celsius)
+    {
+        vector<double> result;
+        double Kelvin, Fahrenheit;
+        Kelvin = celsius + 273.15;
+        Fahrenheit = celsius * 1.80 + 32.00;
+        result.push_back(Kelvin);
+        result.push_back(Fahrenheit);
+        return result;
+    }
+};
