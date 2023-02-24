bd4919abd7d186a2208551b6cadba2ce8faf473c
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2023-02-24 12:34:09 +0530

    Create 2221.Find_Triangular_Sum_of_an_Array.cpp

---
+++ 2221.Find_Triangular_Sum_of_an_Array.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int triangularSum(vector<int> &nums)
+    {
+        while (nums.size() != 1)
+        {
+            for (int i = 0; i < nums.size() - 1; i++)
+            {
+                nums[i] = (nums[i] + nums[i + 1]) % 10;
+            }
+            nums.pop_back();
+        }
+        for (int i = 0; i < nums.size(); i++)
+        {
+            cout << nums[i] << " ";
+        }
+        return nums[0];
+    }
+};
