02544192d747694d804fc4814d222838f5a0d29e
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2023-03-12 13:49:11 +0530

    Create 1346.Check_If_N_and_Its_Double_Exist.cpp

---
+++ 1346.Check_If_N_and_Its_Double_Exist.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    bool checkIfExist(vector<int> &arr)
+    {
+        bool result = false;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            for (int j = i + 1; j < arr.size(); j++)
+            {
+                if (arr[j] == 2 * arr[i] || 2 * arr[j] == arr[i])
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
