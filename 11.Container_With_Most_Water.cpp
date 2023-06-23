a7373e959e28f4134ed819d0d9c235093ddcc8d3
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2023-06-23 21:09:03 +0530

    Create 11.Container_With_Most_Water.cpp

---
+++ 11.Container_With_Most_Water.cpp
@@ -0,0 +1,71 @@
+class Solution
+{
+public:
+    int maxArea(vector<int> &height)
+    {
+        int i = 0, j = height.size() - 1;
+        int max_water = 0;
+
+        while (i < j)
+        {
+
+            int h = min(height[i], height[j]);
+            int current_water = (j - i) * (min(height[i], height[j]));
+            max_water = max(max_water, current_water);
+
+            while (height[i] <= h && i < j)
+                i++;
+            while (height[j] <= h && i < j)
+                j--;
+        }
+
+        return max_water;
+    }
+};
+
+// class Solution {
+// public:
+//     int maxArea(vector<int>& height) {
+//         int i=0,j=height.size()-1;
+//         int area = 0;
+
+//         while(i<j){
+//             int new_area = (j-i) * (min(height[i],height[j]));
+
+//             if(new_area > area){
+//                 area=new_area;
+//             }
+
+//             if(height[i]<height[j]){
+//                 i++;
+//             }
+//             else{
+//                 j--;
+//             }
+
+//         }
+
+//         return area;
+//     }
+// };
+
+// brute force solution
+
+// class Solution {
+// public:
+//     int maxArea(vector<int>& height) {
+//         int n = height.size();
+//         int area = 0;
+
+//         for(int i=0;i<n;i++){
+//             for(int j=i+1;j<n;j++){
+
+//                 int new_area = (j-i) * (min(height[i],height[j]));
+//                 if(new_area > area){
+//                     area=new_area;
+//                 }
+//             }
+//         }
+//         return area;
+//     }
+// };
