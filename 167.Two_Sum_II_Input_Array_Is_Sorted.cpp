85629cdbb38c1528bb66e9542f7ef490d3fd597b
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2023-06-23 21:09:08 +0530

    Update 167.Two_Sum_II_Input_Array_Is_Sorted.cpp

--- 167.Two_Sum_II_Input_Array_Is_Sorted.cpp
+++ 167.Two_Sum_II_Input_Array_Is_Sorted.cpp
@@ -7,20 +7,47 @@ public:
         int j = numbers.size() - 1;
         while (i < j)
         {
-            int sum = numbers[i] + numbers[j];
-            if (sum < target)
-            {
-                i++;
-            }
-            else if (sum > target)
-            {
-                j--;
-            }
-            else
+
+            if (numbers[i] + numbers[j] == target)
             {
                 return vector{i + 1, j + 1};
             }
+            else if (numbers[i] + numbers[j] < target)
+            {
+                i++;
+            }
+            else if (numbers[i] + numbers[j] > target)
+            {
+                j--;
+            }
         }
-        return vector{-1, -1};
+        return {};
     }
 };
+
+// class Solution
+// {
+// public:
+//     vector<int> twoSum(vector<int> &numbers, int target)
+//     {
+//         int i = 0;
+//         int j = numbers.size() - 1;
+//         while (i < j)
+//         {
+//             int sum = numbers[i] + numbers[j];
+//             if (sum < target)
+//             {
+//                 i++;
+//             }
+//             else if (sum > target)
+//             {
+//                 j--;
+//             }
+//             else
+//             {
+//                 return vector{i + 1, j + 1};
+//             }
+//         }
+//         return vector{-1, -1};
+//     }
+// };
