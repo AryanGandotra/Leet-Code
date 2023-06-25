2f8140ce09b07ff41c124a98e4bc99779a5bb230
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2023-06-25 15:31:09 +0530

    Update 20.Valid_Parentheses.cpp

--- 20.Valid_Parentheses.cpp
+++ 20.Valid_Parentheses.cpp
@@ -12,14 +12,16 @@ public:
 
         for (int i = 0; i < s.length(); i++)
         {
+            // input string start from a closing bracket
             if (input.empty() && (s[i] == '}' || s[i] == ']' || s[i] == ')'))
             {
                 return false;
             }
+
+            // if an opening bracket is present on the top and a closing bracket comes in the string while traversing we pop out the top else we push the element in the stack
+
             if (s[i] == '}' && input.top() == '{' || s[i] == ']' && input.top() == '[' || s[i] == ')' && input.top() == '(')
             {
-                input.push(s[i]);
-                input.pop();
                 input.pop();
             }
             else
@@ -28,14 +30,12 @@ public:
             }
         }
 
+
+        // if the stack is empty we say it is a valid paranthesis else it is not
         if (input.empty())
         {
             return true;
         }
-
-        else
-        {
-            return false;
-        }
+        return false;
     }
 };
