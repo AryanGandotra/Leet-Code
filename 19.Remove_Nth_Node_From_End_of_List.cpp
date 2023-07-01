02587f21af4fcabace3f833c6c8e0c09dd99944c
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2023-07-01 15:29:56 +0530

    Update 19.Remove_Nth_Node_From_End_of_List.cpp

--- 19.Remove_Nth_Node_From_End_of_List.cpp
+++ 19.Remove_Nth_Node_From_End_of_List.cpp
@@ -11,46 +11,109 @@
 class Solution
 {
 public:
-    ListNode *reverse(ListNode *head)
-    {
-        ListNode *r = NULL;
-        ListNode *p = head;
-        ListNode *q;
-        while (p != NULL)
-        {
-            q = p->next;
-            p->next = r;
-            r = p;
-            p = q;
-        }
-        return r;
-    }
     ListNode *removeNthFromEnd(ListNode *head, int n)
     {
-        if ((head->next == NULL && n == 1) || head == NULL)
+        ListNode *temp = NULL;
+        ListNode *left = head;
+        ListNode *right = head;
+
+        while (n > 0 && right != NULL)
         {
-            return NULL;
+            right = right->next;
+            n--;
         }
-        head = reverse(head);
-        ListNode *p = head;
-        ListNode *q = head;
-        if (n == 1)
+
+        if (!right)
+            return head->next;
+
+        while (right != NULL)
         {
-            head = head->next;
+            temp = left;
+            left = left->next;
+            right = right->next;
         }
-        else
-        {
-            for (int i = 0; i < n - 2; i++)
-            {
-                p = p->next;
-            }
-            for (int i = 0; i < n - 1; i++)
-            {
-                q = q->next;
-            }
-            p->next = q->next;
-        }
-        head = reverse(head);
+        temp->next = left->next;
+        delete left;
+
         return head;
     }
 };
+
+// class Solution {
+// public:
+//     int getLength(ListNode* head){
+//         int count = 0;
+
+//         while(head!=NULL){
+//             count++;
+//             head=head->next;
+//         }
+//         return count;
+//     }
+
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+//         if(head==NULL || head->next==NULL && n==1){
+//             return NULL;
+//         }
+
+//         int count = getLength(head);
+
+//         if(n==count){
+//             head = head->next;
+//             return head;
+//         }
+
+//         ListNode* curr = head;
+//         ListNode* prev=NULL;
+
+//         for(int i=0;i<(count-n);i++){
+//             prev=curr;
+//             curr=curr->next;
+//         }
+
+//         prev->next = curr->next;
+//         curr->next = NULL;
+
+//         return head;
+//     }
+// };
+
+// By reversing the linked list
+
+// class Solution {
+// public:
+//     ListNode* reverse(ListNode* head){
+//         ListNode* r=NULL;
+//         ListNode* p=head;
+//         ListNode* q;
+//         while(p!=NULL){
+//             q=p->next;
+//             p->next=r;
+//             r=p;
+//             p=q;
+//         }
+//         return r;
+//     }
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+//         if((head->next==NULL && n==1)||head==NULL){
+//             return NULL;
+//         }
+//         head=reverse(head);
+//         ListNode* p=head;
+//         ListNode* q=head;
+//         if(n==1){
+//             head=head->next;
+//         }
+//         else{
+//             for(int i=0;i<n-2;i++){
+//             p=p->next;
+//         }
+//         for(int i=0;i<n-1;i++){
+//             q=q->next;
+//         }
+//         p->next=q->next;
+//         }
+//         head=reverse(head);
+//         return head;
+//     }
+// };
