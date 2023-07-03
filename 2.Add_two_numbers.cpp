ee1f9371abf4777e632f1ef94364097243315ee1
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2023-07-03 14:12:55 +0530

    Update 2.Add_two_numbers.cpp

--- 2.Add_two_numbers.cpp
+++ 2.Add_two_numbers.cpp
@@ -12,41 +12,18 @@
 class Solution
 {
 public:
-    ListNode *biggest(ListNode *l1, ListNode *l2)
-    {
-        int count1 = 0, count2 = 0;
-        ListNode *p, *q;
-        p = l1;
-        q = l2;
-        while (p)
-        {
-            count1++;
-            p = p->next;
-        }
-        while (q)
-        {
-            count2++;
-            q = q->next;
-        }
-        if (count1 >= count2)
-        {
-            return l1;
-        }
-        return l2;
-    }
-
-    void insert(ListNode **root, int item)
+    void insert(ListNode *&root, int item)
     {
         ListNode *temp = new ListNode;
         ListNode *ptr;
         temp->val = item;
         temp->next = NULL;
 
-        if (*root == NULL)
-            *root = temp;
+        if (root == NULL)
+            root = temp;
         else
         {
-            ptr = *root;
+            ptr = root;
             while (ptr->next != NULL)
                 ptr = ptr->next;
             ptr->next = temp;
@@ -64,7 +41,7 @@ public:
         {
             sum = p->val + q->val + carry;
             digit = sum % 10;
-            insert(&temp, digit);
+            insert(temp, digit);
             carry = sum / 10;
             p = p->next;
             q = q->next;
@@ -73,7 +50,7 @@ public:
         {
             digit = (p->val + carry) % 10;
             carry = (p->val + carry) / 10;
-            insert(&temp, digit);
+            insert(temp, digit);
             p = p->next;
         }
 
@@ -81,13 +58,13 @@ public:
         {
             digit = (q->val + carry) % 10;
             carry = (q->val + carry) / 10;
-            insert(&temp, digit);
+            insert(temp, digit);
             q = q->next;
         }
 
         if (carry > 0)
         {
-            insert(&temp, carry);
+            insert(temp, carry);
             carry = 0;
         }
 
