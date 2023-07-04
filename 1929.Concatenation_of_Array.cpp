647074a8e202a4195ccee01c7e4715dcd693c811
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2023-07-04 15:06:57 +0530

    Update 1929.Concatenation_of_Array.cpp

--- 1929.Concatenation_of_Array.cpp
+++ 1929.Concatenation_of_Array.cpp
@@ -3,11 +3,24 @@ class Solution
 public:
     vector<int> getConcatenation(vector<int> &nums)
     {
-        int size = nums.size();
-        for (int i = 0; i < size; i++)
+        vector<int> ans;
+        int len;
+        len = nums.size();
+        for (int i = 0; i < 2 * len; i++)
         {
-            nums.push_back(nums[i]);
+            ans.push_back(nums[i % len]);
         }
-        return nums;
+        return ans;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> getConcatenation(vector<int>& nums) {
+//         int size=nums.size();
+//         for(int i=0;i<size;i++){
+//             nums.push_back(nums[i]);
+//         }
+//         return nums;
+//     }
+// };
