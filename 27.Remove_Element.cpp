86f39ef263d7a065c0bb5ef7be27d6cb1d481532
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2023-07-05 13:46:16 +0530

    Update 27.Remove_Element.cpp

--- 27.Remove_Element.cpp
+++ 27.Remove_Element.cpp
@@ -3,19 +3,34 @@ class Solution
 public:
     int removeElement(vector<int> &nums, int val)
     {
-        vector<int> result;
-        if (nums.size() == 0)
-        {
-            return 0;
-        }
-        for (int i = 0; i < nums.size(); i++)
+        int n = nums.size();
+        int count = 0;
+        for (int i = 0; i < n; i++)
         {
             if (nums[i] != val)
             {
-                result.push_back(nums[i]);
+                swap(nums[i], nums[count]);
+                count++;
             }
         }
-        nums = result;
-        return nums.size();
+
+        return count;
     }
 };
+
+// class Solution {
+// public:
+//     int removeElement(vector<int>& nums, int val) {
+//         vector<int> result;
+//         if(nums.size()==0){
+//             return 0;
+//         }
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]!=val){
+//                 result.push_back(nums[i]);
+//             }
+//         }
+//         nums=result;
+//         return nums.size();
+//     }
+// };
