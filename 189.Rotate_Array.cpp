08d4054a7668de1af69569a9661e72111d7cad9f
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2023-07-08 21:09:41 +0530

    Update 189.Rotate_Array.cpp

--- 189.Rotate_Array.cpp
+++ 189.Rotate_Array.cpp
@@ -3,15 +3,51 @@ class Solution
 public:
     void rotate(vector<int> &nums, int k)
     {
-        vector<int> arr2(nums.size());
-        for (int i = 0; i < nums.size(); i++)
+
+        k %= nums.size();
+
+        int i = 0, j = nums.size() - 1;
+
+        while (i <= j)
         {
-            arr2[(i + k) % nums.size()] = nums[i];
+            swap(nums[i], nums[j]);
+            i++;
+            j--;
         }
 
-        for (int i = 0; i < nums.size(); i++)
+        int n = 0, m = k - 1;
+
+        while (n <= m)
         {
-            nums[i] = arr2[i];
+            swap(nums[n], nums[m]);
+            n++;
+            m--;
+        }
+
+        n = k;
+        m = nums.size() - 1;
+
+        while (n <= m)
+        {
+            swap(nums[n], nums[m]);
+            n++;
+            m--;
         }
     }
 };
+
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+//     vector<int>arr2(nums.size());
+//     for (int i = 0; i < nums.size(); i++)
+//     {
+//         arr2[(i + k) % nums.size()] = nums[i];
+//     }
+
+//     for (int i = 0; i < nums.size(); i++)
+//     {
+//         nums[i] = arr2[i];
+//     }
+//     }
+// };
