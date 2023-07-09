77b7ebf457706553400b0a5ac1a80b3a671fb427
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2023-07-09 16:14:21 +0530

    Update 80.Remove_Duplicate_from_Sorted_Array_II.cpp

--- 80.Remove_Duplicate_from_Sorted_Array_II.cpp
+++ 80.Remove_Duplicate_from_Sorted_Array_II.cpp
@@ -1,28 +1,50 @@
+// class Solution
+// {
+// public:
+//     int removeDuplicates(vector<int> &nums)
+//     {
+//         int left = 0, right = 0;
+
+//         while (right < nums.size())
+//         {
+//             int count = 1;
+
+//             while (right + 1 < nums.size() && nums[right] == nums[right + 1])
+//             {
+//                 right++;
+//                 count++;
+//             }
+
+//             for (int i = 0; i < min(2, count); i++)
+//             {
+//                 nums[left] = nums[right];
+//                 left++;
+//             }
+//             right++;
+//         }
+
+//         return left;
+//     }
+// };
+
 class Solution
 {
 public:
     int removeDuplicates(vector<int> &nums)
     {
-        int left = 0, right = 0;
+        int n = nums.size();
+        if (n < 3)
+            return n;
 
-        while (right < nums.size())
+        int indx = 2;
+        for (int i = 2; i < nums.size(); i++)
         {
-            int count = 1;
-
-            while (right + 1 < nums.size() && nums[right] == nums[right + 1])
+            if (nums[i] != nums[indx - 2])
             {
-                right++;
-                count++;
+                nums[indx] = nums[i];
+                indx++;
             }
-
-            for (int i = 0; i < min(2, count); i++)
-            {
-                nums[left] = nums[right];
-                left++;
-            }
-            right++;
         }
-
-        return left;
+        return indx;
     }
 };
