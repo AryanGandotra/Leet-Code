64b6145eef9cc8d10417c7b289dfe0dd72d92941
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2023-07-24 17:51:28 +0530

    Update 75.Sort_Colors.cpp

--- 75.Sort_Colors.cpp
+++ 75.Sort_Colors.cpp
@@ -3,6 +3,24 @@ class Solution
 public:
     void sortColors(vector<int> &nums)
     {
-        sort(nums.begin(), nums.end());
+        int count[3] = {0};
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            count[nums[i]]++;
+        }
+
+        int index = 0;
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (count[i] > 0)
+                while (count[i] > 0)
+                {
+                    nums[index] = i;
+                    index++;
+                    count[i]--;
+                }
+        }
     }
 };
