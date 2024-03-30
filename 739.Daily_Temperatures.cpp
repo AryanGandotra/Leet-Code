dc518f6f72e02774b1392ed7b4155d659eb285e1
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2024-03-30 18:40:30 +0530

    updated 705.Design_HashSet.cpp

--- 739.Daily_Temperatures.cpp
+++ 739.Daily_Temperatures.cpp
@@ -1,24 +1,80 @@
-class Solution
+class MyHashSet
 {
+private:
+    vector<list<int>> hashSet;
+    int size = 1e6 + 1;
+
 public:
-    vector<int> dailyTemperatures(vector<int> &temperatures)
+    MyHashSet()
     {
-        int n = temperatures.size();
-        vector<int> result(n, 0);
+        hashSet.resize(size);
+    }
 
-        stack<int> s;
+    int hash(int key)
+    {
+        return key % size;
+    }
 
-        for (int i = 0; i < n; i++)
-        {
-            while (!s.empty() && temperatures[s.top()] < temperatures[i])
-            {
-                int j = s.top();
-                result[j] = i - j;
-                s.pop();
-            }
-            s.push(i);
-        }
+    list<int>::iterator Search(int key)
+    {
+        int i = hash(key);
+        return find(hashSet[i].begin(), hashSet[i].end(), key);
+    }
 
-        return result;
+    void add(int key)
+    {
+        if (contains(key))
+            return;
+
+        int i = hash(key);
+        hashSet[i].push_back(key);
+    }
+
+    void remove(int key)
+    {
+        if (!contains(key))
+            return;
+        int i = hash(key);
+        hashSet[i].erase(Search(key));
+    }
+
+    bool contains(int key)
+    {
+        int i = hash(key);
+
+        if (Search(key) != hashSet[i].end())
+            return true;
+        return false;
     }
 };
+
+// class MyHashSet {
+// private:
+// vector<int> set;
+// public:
+
+//     MyHashSet() {
+//         set.resize(1000001, false);
+//     }
+
+//     void add(int key) {
+//         set[key] = true;
+//     }
+
+//     void remove(int key) {
+//         set[key] = false;
+//     }
+
+//     bool contains(int key) {
+//         return set[key];
+
+//     }
+// };
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
