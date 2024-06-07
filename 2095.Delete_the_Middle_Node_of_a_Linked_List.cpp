65d574aadd8c124668d46c9e0cc9c39c8c1d13ed
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2024-06-07 15:17:32 +0530

    Update 2095.Delete_the_Middle_Node_of_a_Linked_List.cpp

--- 2095.Delete_the_Middle_Node_of_a_Linked_List.cpp
+++ 2095.Delete_the_Middle_Node_of_a_Linked_List.cpp
@@ -11,34 +11,62 @@
 class Solution
 {
 public:
-    int count(ListNode *head)
+    pair<ListNode *, ListNode *> getMiddle(ListNode *head)
     {
-        ListNode *p = head;
-        int count = 0;
-        while (p)
+        ListNode *slow = head, *fast = head, *prevToMid = NULL;
+
+        while (fast != NULL && fast->next != NULL)
         {
-            count++;
-            p = p->next;
+            prevToMid = slow;
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        return count;
+
+        return {slow, prevToMid};
     }
     ListNode *deleteMiddle(ListNode *head)
     {
-        if (head == NULL)
+        if (head == NULL || head->next == NULL)
             return NULL;
-        if (head->next == NULL)
-        {
-            delete head;
-            return NULL;
-        }
-        int length = count(head);
-        ListNode *p = head;
-        int mid = length / 2;
-        while (mid-- > 1)
-        {
-            head = head->next;
-        }
-        head->next = head->next->next;
-        return p;
+        ListNode *Middle = getMiddle(head).first;
+        ListNode *PrevToMiddle = getMiddle(head).second;
+
+        PrevToMiddle->next = Middle->next;
+        delete Middle;
+        return head;
     }
 };
+// class Solution
+// {
+// public:
+//     int count(ListNode *head)
+//     {
+//         ListNode *p = head;
+//         int count = 0;
+//         while (p)
+//         {
+//             count++;
+//             p = p->next;
+//         }
+//         return count;
+//     }
+//     ListNode *deleteMiddle(ListNode *head)
+//     {
+//         if (head == NULL)
+//             return NULL;
+//         if (head->next == NULL)
+//         {
+//             delete head;
+//             return NULL;
+//         }
+//         int length = count(head);
+//         ListNode *p = head;
+//         int mid = length / 2;
+//         while (mid-- > 1)
+//         {
+//             head = head->next;
+//         }
+//         head->next = head->next->next;
+//         return p;
+//     }
+// };
