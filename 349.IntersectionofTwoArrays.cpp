1ef3bb81abe17ee9eb98afd057ff2eb7a7fd5e31
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2024-07-02 19:59:13 +0530

    Create 349.IntersectionofTwoArrays.cpp

---
+++ 349.IntersectionofTwoArrays.cpp
@@ -0,0 +1,36 @@
+class Solution
+{
+public:
+    vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
+    {
+        vector<int> result;
+
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        int i = 0, j = 0;
+
+        while (i < nums1.size() && j < nums2.size())
+        {
+
+            if (nums1[i] == nums2[j])
+            {
+                if (result.empty() || nums1[i] != result.back())
+                {
+                    result.push_back(nums1[i]);
+                    cout << nums1[i] << endl;
+                }
+                i++;
+                j++;
+            }
+
+            else if (nums1[i] < nums2[j])
+                i++;
+
+            else if (nums2[j] < nums1[i])
+                j++;
+        }
+
+        return result;
+    }
+};
