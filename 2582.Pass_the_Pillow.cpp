513606a8b29e2b16d8e98f8cd30ceb3cb507fd00
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2024-07-06 16:16:38 +0530

    Update 2582.Pass_the_Pillow.cpp

--- 2582.Pass_the_Pillow.cpp
+++ 2582.Pass_the_Pillow.cpp
@@ -3,22 +3,21 @@ class Solution
 public:
     int passThePillow(int n, int time)
     {
-        bool changeDirection = false;
+        if (time < n)
+            return time + 1;
+        else if (time == n)
+            return n - 1;
 
-        int ans = 1;
+        int rounds = time / (n - 1);
 
-        for (int i = 0; i < time; i++)
+        if (rounds % 2 == 0)
         {
-            if (i % (n - 1) == 0 && i != 0)
-                changeDirection = !changeDirection;
-
-            if (changeDirection == false)
-                ans++;
-            else
-                ans--;
+            int movesLeft = time - (n - 1) * rounds;
+            return movesLeft + 1;
         }
 
-        return ans;
+        int timeLeft = time - (n - 1) * rounds;
+        return n - timeLeft;
     }
 };
 
@@ -27,20 +26,21 @@ public:
 // public:
 //     int passThePillow(int n, int time)
 //     {
-//         if (time < n)
-//             return time + 1;
-//         else if (time == n)
-//             return n - 1;
+//         bool changeDirection = false;
 
-//         int rounds = time / (n - 1);
+//         int ans = 1;
 
-//         if (rounds % 2 == 0)
+//         for (int i = 0; i < time; i++)
 //         {
-//             int movesLeft = time - (n - 1) * rounds;
-//             return movesLeft + 1;
+//             if (i % (n - 1) == 0 && i != 0)
+//                 changeDirection = !changeDirection;
+
+//             if (changeDirection == false)
+//                 ans++;
+//             else
+//                 ans--;
 //         }
 
-//         int timeLeft = time - (n - 1) * rounds;
-//         return n - timeLeft;
+//         return ans;
 //     }
 // };
