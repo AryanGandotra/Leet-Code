e7ab4986f0f60244e2bad632b25e19e4a7c06b75
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2024-07-06 16:11:19 +0530

    Update 2582.Pass_the_Pillow.cpp

--- 2582.Pass_the_Pillow.cpp
+++ 2582.Pass_the_Pillow.cpp
@@ -3,20 +3,44 @@ class Solution
 public:
     int passThePillow(int n, int time)
     {
-        if (time < n)
-            return time + 1;
-        else if (time == n)
-            return n - 1;
+        bool changeDirection = false;
 
-        int rounds = time / (n - 1);
+        int ans = 1;
 
-        if (rounds % 2 == 0)
+        for (int i = 0; i < time; i++)
         {
-            int movesLeft = time - (n - 1) * rounds;
-            return movesLeft + 1;
+            if (i % (n - 1) == 0 && i != 0)
+                changeDirection = !changeDirection;
+
+            if (changeDirection == false)
+                ans++;
+            else
+                ans--;
         }
 
-        int timeLeft = time - (n - 1) * rounds;
-        return n - timeLeft;
+        return ans;
     }
 };
+
+// class Solution
+// {
+// public:
+//     int passThePillow(int n, int time)
+//     {
+//         if (time < n)
+//             return time + 1;
+//         else if (time == n)
+//             return n - 1;
+
+//         int rounds = time / (n - 1);
+
+//         if (rounds % 2 == 0)
+//         {
+//             int movesLeft = time - (n - 1) * rounds;
+//             return movesLeft + 1;
+//         }
+
+//         int timeLeft = time - (n - 1) * rounds;
+//         return n - timeLeft;
+//     }
+// };
