27351b0146ce1dd893cf4e8499fab167208e00b1
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2024-07-06 17:14:36 +0530

    Update 155.Min_Stack.cpp

--- 155.Min_Stack.cpp
+++ 155.Min_Stack.cpp
@@ -33,3 +33,12 @@ public:
         return st.top().second;
     }
 };
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
