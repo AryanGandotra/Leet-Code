b57dcfb55e33dc95b97ce5b061fe500b410ab202
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2024-07-11 17:51:11 +0530

    Update 1190.Reverse_Substrings_Between_Each_Pair_of_Parentheses.cpp

--- 1190.Reverse_Substrings_Between_Each_Pair_of_Parentheses.cpp
+++ 1190.Reverse_Substrings_Between_Each_Pair_of_Parentheses.cpp
@@ -3,25 +3,67 @@ class Solution
 public:
     string reverseParentheses(string s)
     {
-        stack<int> st;
-        string result;
+        int n = s.length();
 
-        for (int i = 0; i < s.length(); i++)
+        stack<int> st;
+        vector<int> arr(n);
+
+        for (int i = 0; i < n; i++)
         {
             if (s[i] == '(')
-                st.push(result.length());
+                st.push(i);
             else if (s[i] == ')')
             {
-                int start = st.top();
+                int j = st.top();
                 st.pop();
-                reverse(result.begin() + start, result.end());
+                arr[i] = j;
+                arr[j] = i;
+            }
+        }
+
+        string result = "";
+        int flag = 1;
+
+        for (int i = 0; i < n; i += flag)
+        {
+            if (s[i] == '(' || s[i] == ')')
+            {
+                i = arr[i];
+                flag = -flag;
             }
             else
             {
                 result += s[i];
             }
         }
-
         return result;
     }
 };
+
+// class Solution
+// {
+// public:
+//     string reverseParentheses(string s)
+//     {
+//         stack<int> st;
+//         string result;
+
+//         for (int i = 0; i < s.length(); i++)
+//         {
+//             if (s[i] == '(')
+//                 st.push(result.length());
+//             else if (s[i] == ')')
+//             {
+//                 int start = st.top();
+//                 st.pop();
+//                 reverse(result.begin() + start, result.end());
+//             }
+//             else
+//             {
+//                 result += s[i];
+//             }
+//         }
+
+//         return result;
+//     }
+// };
