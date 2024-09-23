d4fad6d5242c4a8de30a105c5413695b369e9e6d
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2024-09-23 23:47:33 +0530

    Create Top view of Binary Tree.cpp

---
+++ Top view of Binary Tree.cpp
@@ -0,0 +1,49 @@
+class Solution
+{
+public:
+
+    vector<int> topView(Node *root)
+    {
+        vector<int> result;
+
+        if (root == NULL)
+            return result;
+
+        //  horizontal Distance -> val
+        map<int, int> Nodes;
+
+        queue<pair<Node *, int>> q;
+
+        q.push({root, 0});
+
+        while (!q.empty())
+        {
+            Node *temp = q.front().first;
+            int horizontalDistance = q.front().second;
+
+            q.pop();
+
+            if (Nodes.find(horizontalDistance) == Nodes.end())
+            {
+                Nodes[horizontalDistance] = temp->data;
+            }
+
+            if (temp->left)
+            {
+                q.push({temp->left, horizontalDistance - 1});
+            }
+
+            if (temp->right)
+            {
+                q.push({temp->right, horizontalDistance + 1});
+            }
+        }
+
+        for (auto node : Nodes)
+        {
+            result.push_back(node.second);
+        }
+
+        return result;
+    }
+};
