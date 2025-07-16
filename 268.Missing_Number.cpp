b6acb3a550a58b7c72a2d40adea79bd7e0be2e3d
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2025-07-16 19:24:25 +0530

    Update 268.Missing_Number.cpp

--- 268.Missing_Number.cpp
+++ 268.Missing_Number.cpp
@@ -4,24 +4,14 @@ public:
     int missingNumber(vector<int> &nums)
     {
         int n = nums.size();
-        int *arr = new int[n + 1];
-        for (int i = 0; i <= n; i++)
+        int actualSum = 0;
+        int expectedSum = (n * (n + 1)) / 2;
+
+        for (int i = 0; i < nums.size(); i++)
         {
-            arr[i] = 0;
-        }
-        for (int i = 0; i < n; i++)
-        {
-            arr[nums[i]] = arr[nums[i]] + 1;
-        }
-        int i;
-        for (i = 0; i <= n; ++i)
-        {
-            if (arr[i] == 0)
-            {
-                break;
-            }
+            actualSum += nums[i];
         }
 
-        return i;
+        return expectedSum - actualSum;
     }
 };
