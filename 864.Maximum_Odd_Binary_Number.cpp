bdcafd752412d4365f60ac72c53e59b595377f59
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2025-07-22 19:23:21 +0530

    Update 864.Maximum_Odd_Binary_Number.cpp

--- 864.Maximum_Odd_Binary_Number.cpp
+++ 864.Maximum_Odd_Binary_Number.cpp
@@ -12,33 +12,55 @@ public:
                 oneCount++;
         }
 
-        string ans;
-
-        if (oneCount == 1)
-        {
-            for (int i = 0; i < n - 1; i++)
-            {
-                ans += '0';
-            }
-
-            ans += '1';
-            return ans;
-        }
-
-        for (int i = oneCount; i < n; i++)
-        {
-            ans += '0';
-        }
-
+        string ans(oneCount - 1, '1');
+        ans += string(n - oneCount, '0');
         ans += '1';
-        oneCount--;
-
-        while (oneCount > 0)
-        {
-            ans.insert(ans.begin(), '1');
-            oneCount--;
-        }
 
         return ans;
     }
 };
+
+// class Solution
+// {
+// public:
+//     string maximumOddBinaryNumber(string s)
+//     {
+//         int n = s.length();
+//         int oneCount = 0;
+
+//         for (int i = 0; i < n; i++)
+//         {
+//             if (s[i] == '1')
+//                 oneCount++;
+//         }
+
+//         string ans;
+
+//         if (oneCount == 1)
+//         {
+//             for (int i = 0; i < n - 1; i++)
+//             {
+//                 ans += '0';
+//             }
+
+//             ans += '1';
+//             return ans;
+//         }
+
+//         for (int i = oneCount; i < n; i++)
+//         {
+//             ans += '0';
+//         }
+
+//         ans += '1';
+//         oneCount--;
+
+//         while (oneCount > 0)
+//         {
+//             ans.insert(ans.begin(), '1');
+//             oneCount--;
+//         }
+
+//         return ans;
+//     }
+// };
