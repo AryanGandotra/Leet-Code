d9c1ce5b677ed45820779d66dc9fd43df9c945be
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2025-07-26 19:47:30 +0530

    Update 424.Longest_Repeating_Character_Replacement.cpp

--- 424.Longest_Repeating_Character_Replacement.cpp
+++ 424.Longest_Repeating_Character_Replacement.cpp
@@ -13,7 +13,7 @@ public:
             mp[s[j]]++;
             maxFreq = max(maxFreq, mp[s[j]]);
 
-            while ((j - i + 1) - maxFreq > k)
+            if ((j - i + 1) - maxFreq > k)
             {
                 mp[s[i]]--;
                 i++;
@@ -26,3 +26,32 @@ public:
         return maxLength;
     }
 };
+
+// class Solution
+// {
+// public:
+//     int characterReplacement(string s, int k)
+//     {
+//         int i = 0, j = 0;
+//         int maxLength = 0;
+//         int maxFreq = 0;
+//         map<char, int> mp;
+
+//         while (j < s.length())
+//         {
+//             mp[s[j]]++;
+//             maxFreq = max(maxFreq, mp[s[j]]);
+
+//             while ((j - i + 1) - maxFreq > k)
+//             {
+//                 mp[s[i]]--;
+//                 i++;
+//             }
+
+//             maxLength = max(maxLength, j - i + 1);
+//             j++;
+//         }
+
+//         return maxLength;
+//     }
+// };
