3103b090caad1abd096d6a72fdcbc1148b2161e7
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2025-07-26 14:45:24 +0530

    Update 1004.Max_Consecutive_Ones_III.cpp

--- 1004.Max_Consecutive_Ones_III.cpp
+++ 1004.Max_Consecutive_Ones_III.cpp
@@ -14,17 +14,52 @@ public:
                 zeroCount++;
             }
 
-            while (zeroCount > k)
+            if (zeroCount > k)
             {
                 if (nums[i] == 0)
                     zeroCount--;
                 i++;
             }
 
-            maxLength = max(maxLength, j - i + 1);
+            if (zeroCount <= k)
+            {
+                maxLength = max(maxLength, j - i + 1);
+            }
+
             j++;
         }
 
         return maxLength;
     }
 };
+
+// class Solution
+// {
+// public:
+//     int longestOnes(vector<int> &nums, int k)
+//     {
+//         int i = 0, j = 0;
+//         int maxLength = 0;
+//         int zeroCount = 0;
+
+//         while (j < nums.size())
+//         {
+//             if (nums[j] == 0)
+//             {
+//                 zeroCount++;
+//             }
+
+//             while (zeroCount > k)
+//             {
+//                 if (nums[i] == 0)
+//                     zeroCount--;
+//                 i++;
+//             }
+
+//             maxLength = max(maxLength, j - i + 1);
+//             j++;
+//         }
+
+//         return maxLength;
+//     }
+// };
