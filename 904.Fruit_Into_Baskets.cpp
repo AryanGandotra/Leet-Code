080f08a8aa2f6768c032cde8de8f98ff14e9c276
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2025-07-26 14:46:20 +0530

    Update 904.Fruit_Into_Baskets.cpp

--- 904.Fruit_Into_Baskets.cpp
+++ 904.Fruit_Into_Baskets.cpp
@@ -12,7 +12,7 @@ public:
         {
             mp[fruits[j]]++;
 
-            while (mp.size() > 2)
+            if (mp.size() > 2)
             {
                 mp[fruits[i]]--;
                 if (mp[fruits[i]] <= 0)
@@ -30,3 +30,36 @@ public:
         return maxLength;
     }
 };
+
+// class Solution
+// {
+// public:
+//     int totalFruit(vector<int> &fruits)
+//     {
+
+//         int i = 0, j = 0;
+//         int maxLength = 0;
+//         map<int, int> mp;
+
+//         while (j < fruits.size())
+//         {
+//             mp[fruits[j]]++;
+
+//             while (mp.size() > 2)
+//             {
+//                 mp[fruits[i]]--;
+//                 if (mp[fruits[i]] <= 0)
+//                     mp.erase(fruits[i]);
+//                 i++;
+//             }
+//             if (mp.size() <= 2)
+//             {
+//                 maxLength = max(maxLength, j - i + 1);
+//             }
+
+//             j++;
+//         }
+
+//         return maxLength;
+//     }
+// };
