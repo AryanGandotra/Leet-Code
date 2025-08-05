a37897e67fbe2a94eafa6138ebd3cfc415494b12
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2025-08-05 17:56:11 +0530

    Update 1812.Determine_Color_of_a_Square.cpp

--- 1812.Determine_Color_of_a_Square.cpp
+++ 1812.Determine_Color_of_a_Square.cpp
@@ -3,18 +3,27 @@ class Solution
 public:
     bool squareIsWhite(string coordinates)
     {
-        char letter = coordinates[0];
-        int number = coordinates[1] - '0';
-
-        if ((letter == 'b' || letter == 'd' || letter == 'f' ||
-             letter == 'h') &&
-            number % 2 == 1)
-            return true;
-        else if ((letter == 'a' || letter == 'c' || letter == 'e' ||
-                  letter == 'g') &&
-                 number % 2 == 0)
-            return true;
-
-        return false;
+        return ((coordinates[0] - 'a') + (coordinates[1] - '0')) % 2 == 0;
     }
-};
+}; 
+
+// class Solution
+// {
+// public:
+//     bool squareIsWhite(string coordinates)
+//     {
+//         char letter = coordinates[0];
+//         int number = coordinates[1] - '0';
+
+//         if ((letter == 'b' || letter == 'd' || letter == 'f' ||
+//              letter == 'h') &&
+//             number % 2 == 1)
+//             return true;
+//         else if ((letter == 'a' || letter == 'c' || letter == 'e' ||
+//                   letter == 'g') &&
+//                  number % 2 == 0)
+//             return true;
+
+//         return false;
+//     }
+// };
