472b09fce744a06b5c91a1276ae371bf173c1212
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2025-08-06 17:32:57 +0530

    Update 2089.Find_Target_After_Sorting_Array.cpp

--- 2089.Find_Target_After_Sorting_Array.cpp
+++ 2089.Find_Target_After_Sorting_Array.cpp
@@ -17,3 +17,56 @@ public:
         return ans;
     }
 };
+
+// class Solution
+// {
+// public:
+//     vector<int> targetIndices(vector<int> &nums, int target)
+//     {
+//         vector<int> ans;
+//         sort(nums.begin(), nums.end());
+
+//         int left = 0, right = nums.size() - 1;
+
+//         int first = -1;
+//         while (left <= right)
+//         {
+//             int mid = left + (right - left) / 2;
+//             if (nums[mid] >= target)
+//             {
+//                 right = mid - 1;
+//             }
+//             else
+//             {
+//                 left = mid + 1;
+//             }
+//         }
+//         first = left;
+
+//         left = 0, right = nums.size() - 1;
+//         int last = -1;
+//         while (left <= right)
+//         {
+//             int mid = left + (right - left) / 2;
+//             if (nums[mid] <= target)
+//             {
+//                 left = mid + 1;
+//             }
+//             else
+//             {
+//                 right = mid - 1;
+//             }
+//         }
+//         last = right;
+
+//         for (int i = first; i <= last; ++i)
+//         {
+//             if (i < nums.size() && nums[i] == target)
+//             {
+//                 ans.push_back(i);
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
