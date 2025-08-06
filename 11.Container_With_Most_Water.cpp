f1f415baaaaa8db9b0b5c21b413f5ebdddd1064c
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2025-08-06 17:25:38 +0530

    Update 11.Container_With_Most_Water.cpp

--- 11.Container_With_Most_Water.cpp
+++ 11.Container_With_Most_Water.cpp
@@ -3,25 +3,50 @@ class Solution
 public:
     int maxArea(vector<int> &height)
     {
-        int i = 0, j = height.size() - 1;
-        int max_water = 0;
+        int l = 0, r = height.size() - 1;
 
-        while (i < j)
+        int currArea = 0;
+        int maxArea = 0;
+
+        while (l <= r)
         {
+            currArea = min(height[l], height[r]) * (r - l);
+            maxArea = max(maxArea, currArea);
 
-            int h = min(height[i], height[j]);
-            int current_water = (j - i) * (min(height[i], height[j]));
-            max_water = max(max_water, current_water);
-
-            while (height[i] <= h && i < j)
-                i++;
-            while (height[j] <= h && i < j)
-                j--;
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
         }
 
-        return max_water;
+        return maxArea;
     }
 };
+
+// class Solution
+// {
+// public:
+//     int maxArea(vector<int> &height)
+//     {
+//         int i = 0, j = height.size() - 1;
+//         int max_water = 0;
+
+//         while (i < j)
+//         {
+
+//             int h = min(height[i], height[j]);
+//             int current_water = (j - i) * (min(height[i], height[j]));
+//             max_water = max(max_water, current_water);
+
+//             while (height[i] <= h && i < j)
+//                 i++;
+//             while (height[j] <= h && i < j)
+//                 j--;
+//         }
+
+//         return max_water;
+//     }
+// };
 
 // class Solution {
 // public:
