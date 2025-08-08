16059d1d703dbfe84ef1011b201f9627f292ac55
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2025-08-08 18:05:50 +0530

    Update 901.Online_Stock_Span.cpp

--- 901.Online_Stock_Span.cpp
+++ 901.Online_Stock_Span.cpp
@@ -1,29 +1,53 @@
 class StockSpanner
 {
 public:
-    vector<int> prices;
-    StockSpanner() {}
-
-    int getSmallerElement(int x, int price)
+    stack<pair<int, int>> st;
+    int day = 0;
+    StockSpanner()
     {
-        int ans = 0;
-        for (int i = x; i >= 0; i--)
-        {
-            if (prices[i] <= price)
-                ans++;
-            else
-                break;
-        }
-
-        return ans;
+        int day = 0;
+        st = stack<pair<int, int>>();
     }
 
     int next(int price)
     {
-        prices.push_back(price);
-        return getSmallerElement(prices.size() - 1, price);
+        while (!st.empty() && st.top().first <= price)
+            st.pop();
+
+        int ans = day - (st.empty() ? -1 : st.top().second);
+        st.push({price, day});
+
+        day++;
+        return ans;
     }
 };
+
+// class StockSpanner
+// {
+// public:
+//     vector<int> prices;
+//     StockSpanner() {}
+
+//     int getSmallerElement(int x, int price)
+//     {
+//         int ans = 0;
+//         for (int i = x; i >= 0; i--)
+//         {
+//             if (prices[i] <= price)
+//                 ans++;
+//             else
+//                 break;
+//         }
+
+//         return ans;
+//     }
+
+//     int next(int price)
+//     {
+//         prices.push_back(price);
+//         return getSmallerElement(prices.size() - 1, price);
+//     }
+// };
 
 /**
  * Your StockSpanner object will be instantiated and called as such:
