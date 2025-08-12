5b5dd21932832474090ab4c3f80aa065f5a85dc7
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2025-08-12 17:37:15 +0530

    Update 169.Majority_Element.cpp

--- 169.Majority_Element.cpp
+++ 169.Majority_Element.cpp
@@ -3,48 +3,39 @@ class Solution
 public:
     int majorityElement(vector<int> &nums)
     {
-        unordered_map<int, int> freq;
-        int n = nums.size();
+        int element = nums[0], count = 1;
 
-        for (int i = 0; i < nums.size(); i++)
+        for (int i = 1; i < nums.size(); i++)
         {
-            freq[nums[i]]++;
-            if (freq[nums[i]] > n / 2)
-                return nums[i];
+            if (count == 0)
+            {
+                element = nums[i];
+            }
+            if (nums[i] != element)
+                count--;
+            else
+                count++;
         }
 
-        return -1;
+        return element;
     }
 };
 
-// class Solution {
+// class Solution
+// {
 // public:
-//     int majorityElement(vector<int>& nums) {
-//         int n=nums.size();
-//         int n1=n/2;
-//         cout<<"size of nums: "<<n<<endl;
-//         cout<<"n1: "<<n1<<endl;
-//         int result;
-//         map<int, int> map1;
+//     int majorityElement(vector<int> &nums)
+//     {
+//         unordered_map<int, int> freq;
+//         int n = nums.size();
+
 //         for (int i = 0; i < nums.size(); i++)
 //         {
-//             auto it = map1.find(nums[i]);
-//             if (it == map1.end())
-//             {
-//                 map1.insert(it, pair<int, int>(nums[i], 1));
-//             }
-//             else
-//             {
-//                 it->second++;
-//             }
+//             freq[nums[i]]++;
+//             if (freq[nums[i]] > n / 2)
+//                 return nums[i];
 //         }
-//          for (auto i = map1.begin(); i != map1.end(); i++){
-//              if(i->second > n1){
-//                 result=i->first;
-//              }
-//          }
 
-//          return result;
-
+//         return -1;
 //     }
 // };
