fcf2b97e00b986194a205a1ee023853aa5da649c
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2025-08-15 14:50:40 +0530

    Update 342.Power_of_Four.cpp

--- 342.Power_of_Four.cpp
+++ 342.Power_of_Four.cpp
@@ -3,17 +3,38 @@ class Solution
 public:
     bool isPowerOfFour(int n)
     {
-        for (int i = 0; i < n; i++)
+        int l = 0, r = n;
+
+        while (l <= r)
         {
-            if (pow(4, i) > n)
-            {
-                break;
-            }
-            if (pow(4, i) == n)
-            {
+            int mid = l + (r - l) / 2;
+            if (pow(4, mid) == n)
                 return true;
-            }
+            else if (pow(4, mid) > n)
+                r = mid - 1;
+            else
+                l = mid + 1;
         }
         return false;
     }
 };
+
+// class Solution
+// {
+// public:
+//     bool isPowerOfFour(int n)
+//     {
+//         for (int i = 0; i < n; i++)
+//         {
+//             if (pow(4, i) > n)
+//             {
+//                 break;
+//             }
+//             if (pow(4, i) == n)
+//             {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
