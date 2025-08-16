eceefee7e84b2e6e9d08313f8d9938bd23a56095
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2025-08-16 19:57:34 +0530

    Update 110.Balanced_Binary_Tree.cpp

--- 110.Balanced_Binary_Tree.cpp
+++ 110.Balanced_Binary_Tree.cpp
@@ -1,46 +1,29 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution
 {
 public:
-    pair<bool, int> isBalancedHelper(TreeNode *root)
+    pair<bool, int> helper(TreeNode *root)
     {
         if (root == NULL)
         {
-            pair<bool, int> p = make_pair(true, 0);
-            return p;
+            return {true, 0};
         }
 
-        pair<bool, int> left = isBalancedHelper(root->left);
-        pair<bool, int> right = isBalancedHelper(root->right);
+        pair<bool, int> left = helper(root->left);
+        pair<bool, int> right = helper(root->right);
 
-        bool left_ans = left.first;
-        bool right_ans = right.first;
-        bool difference = abs(left.second - right.second) <= 1;
+        int left_balanced = left.first;
+        int right_balanced = right.first;
+        bool heightDiff = abs(left.second - right.second) <= 1;
 
-        pair<bool, int> ans;
+        if (left_balanced && right_balanced && heightDiff)
+        {
+            return {true, max(left.second, right.second) + 1};
+        }
 
-        if (left_ans && right_ans && difference)
-            ans.first = true;
-        else
-            ans.first = false;
-        ans.second = max(left.second, right.second) + 1;
-
-        return ans;
+        return {false, max(left.second, right.second) + 1};
     }
-    bool isBalanced(TreeNode *root)
-    {
-        return isBalancedHelper(root).first;
-    }
+
+    bool isBalanced(TreeNode *root) { return helper(root).first; }
 };
 
 // class Solution {
