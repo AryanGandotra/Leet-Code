db3c7c01dc4b84c084156a700deb33708ea15dbe
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2025-08-20 21:33:46 +0530

    Update 103.Binary_Tree_Zigzag_Level_Order_Traversal.cpp

--- 103.Binary_Tree_Zigzag_Level_Order_Traversal.cpp
+++ 103.Binary_Tree_Zigzag_Level_Order_Traversal.cpp
@@ -1,52 +1,35 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution
 {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode *root)
     {
-        queue<TreeNode *> q;
-        vector<vector<int>> result;
-
         if (root == NULL)
-            return result;
+            return {};
 
-        bool leftToRight = true;
+        vector<vector<int>> ans;
+        queue<TreeNode *> q;
 
         q.push(root);
+        bool leftToRight = true;
 
         while (!q.empty())
         {
-            int size = q.size();
-            vector<int> ans(size);
-
-            for (int i = 0; i < size; i++)
+            int n = q.size();
+            vector<int> level(n);
+            for (int i = 0; i < n; i++)
             {
                 TreeNode *temp = q.front();
+                int index = leftToRight ? i : n - i - 1;
+                level[index] = temp->val;
                 q.pop();
-                int index = leftToRight ? i : size - i - 1;
-                ans[index] = temp->val;
-
                 if (temp->left)
                     q.push(temp->left);
-
                 if (temp->right)
                     q.push(temp->right);
             }
-
             leftToRight = !leftToRight;
-            result.push_back(ans);
+            ans.push_back(level);
         }
-
-        return result;
+        return ans;
     }
 };
