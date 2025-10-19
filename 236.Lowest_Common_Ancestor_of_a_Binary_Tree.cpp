0814e1f9e4fb0076200a8203099f122e9d975369
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2025-10-19 17:32:02 +0530

    Update 236.Lowest_Common_Ancestor_of_a_Binary_Tree.cpp

--- 236.Lowest_Common_Ancestor_of_a_Binary_Tree.cpp
+++ 236.Lowest_Common_Ancestor_of_a_Binary_Tree.cpp
@@ -1,32 +1,19 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution
 {
 public:
     TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
     {
-        if (root == NULL)
-            return NULL;
-
-        if (root == p || root == q)
+        if (root == NULL || root == p || root == q)
             return root;
 
-        TreeNode *leftN = lowestCommonAncestor(root->left, p, q);
-        TreeNode *rightN = lowestCommonAncestor(root->right, p, q);
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
 
-        if (leftN != NULL && rightN != NULL)
+        if (left == NULL)
+            return right;
+        else if (right == NULL)
+            return left;
+        else
             return root;
-
-        if (leftN != NULL)
-            return leftN;
-
-        return rightN;
     }
 };
